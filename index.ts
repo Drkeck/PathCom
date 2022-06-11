34f4edf38dc36a30996b045b0de7089cc1ac0569
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-06-10 23:26:51 -0400

    little changes

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import express from "express"
 // ^ express ^
+
 const app = express();
 const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("hello")
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
