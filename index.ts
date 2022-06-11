bc933c62ef6a2af42b25598e992cfa35f4fd11e8
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-06-11 00:14:32 -0400

    remembered the flow of mongodbAtlas

--- index.ts
+++ index.ts
@@ -5,13 +5,6 @@ import client from "./Connection/index"
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("hello")
-})
     app.listen(port, () => {
         console.log(`Listening on port ${port}`)
-        client.connect()
-            .then(res => {
-                console.log(res)
-            })
     })
