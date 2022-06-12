f9591292294edb28cbf50dc9f0063098c123611a
Drkeck <k3ckl3y.a@gmail.com>
2022-06-12 12:23:46 -0400

    started building out the skills api section

---
+++ routes/1ed/Jobs/skills.ts
@@ -0,0 +1,14 @@
+import Express from "express";
+const router = Express.Router();
+// ^ router ^ //
+
+router.get("/", (req,res) => {
+    // ^ get all skills ^ //
+    try {
+
+    } catch(error) {
+
+    } finally {
+
+    }
+})
