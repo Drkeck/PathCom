7e64b2819969bf372bed171130f4b4396be5a7ea
Drkeck <k3ckl3y.a@gmail.com>
2022-06-12 12:14:14 -0400

    added some minor changes to how credentials work

--- Connection/index.ts
+++ Connection/index.ts
@@ -1,5 +1,5 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-import { credentials } from "./credentials";
+import credentials from "./credentials";
 const uri = `mongodb+srv://${credentials.username}:${credentials.password}@${credentials.cluster}.jq203po.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {serverApi: ServerApiVersion.v1});
 export default client
